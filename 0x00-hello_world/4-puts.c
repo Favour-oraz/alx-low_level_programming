4ed8bcea5ac2b837314b70502fe4b8938348a3e3
Favour-oraz <favourorazulike9@gmail.com>
2022-12-08 19:15:03 -0800

    work2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- *main - Entry point
- *Return: Always 0 (success)
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0)
-}
+	return (0);
+
+}	
